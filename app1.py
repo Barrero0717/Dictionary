bdf094ec275ddf53f46efc11ada9f9059e1b65be
Andres Felipe Barrero Arce <felipe.barrero17@gmail.com>
2021-09-02 18:41:10 -0500

    Adding Case-Sensitive Words

--- app1.py
+++ app1.py
@@ -1,12 +1,15 @@
 import json
 
+# Load a .json file
 data = json.load(open("data.json"))
 
+# Search a word into the .json file
 def translate(w):
-    if w in data:
-        return data[word]
+    w = w.lower() # Lower case the entered text
+    if w in data: # Ask if the word is into the data
+        return data[w]
     else:
         return "The word does not exist. Please double check it."    
 
 word = input("Enter word: ")
-print(translate(word))    
+print(translate(word))
