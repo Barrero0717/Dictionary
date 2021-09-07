51557254f86c11f67b95c7cce87f35a7d0bf7bb2
Andres Felipe Barrero Arce <felipe.barrero17@gmail.com>
2021-09-07 18:08:57 -0500

    Adding find the Most similar word

--- app1.py
+++ app1.py
@@ -1,4 +1,15 @@
 import json
+from difflib import get_close_matches
+
+"""
+difflib: Helpers for computing deltas
+This module provides classes and functions for comparing sequences. 
+
+get_close_matches(word, possibilities, n=3, cutoff=0.6)
+Return a list of the best “good enough” matches.
+
+URL: https://docs.python.org/3/library/difflib.html
+"""
 
 # Load a .json file
 data = json.load(open("data.json"))
@@ -8,6 +19,8 @@ def translate(w):
     w = w.lower() # Lower case the entered text
     if w in data: # Ask if the word is into the data
         return data[w]
+    elif len(get_close_matches(w, data.keys())) > 0: 
+        return "Did you mean " + get_close_matches(w, data.keys())[0] +" instead?"
     else:
         return "The word does not exist. Please double check it."    
 
